b3a9c5005a05df12ecb0e338414c01a440d5dbf5
Matthew Braziel <bmattcole@gmail.com>
2026-09-23 12:12:31 -0500

    Add grep pattern matching

--- HW3_MatthewBraziel.sh
+++ HW3_MatthewBraziel.sh
@@ -23,3 +23,38 @@ EOF
 
 echo "2. Displaying the contents of FunScript.txt."
 cat FunScript.txt
+echo "3. All lines with alphabet characters :"
+grep '[[:alpha:]]' FunScript.txt
+
+echo "4. All lines with digits :"
+grep '[[:digit:]]' FunScript.txt
+
+echo "5. All lines with lowercase characters :"
+grep '[[:lower:]]' FunScript.txt
+
+echo "6. All lines with uppercase characters :"
+grep '[[:upper:]]' FunScript.txt
+
+echo "7. All lines with alphanumeric characters :"
+grep '[[:alnum:]]' FunScript.txt
+
+echo "8. All lines with punctuation characters :"
+grep '[[:punct:]]' FunScript.txt
+
+echo "9. All the lines with the letter s followed by space :"
+grep 's ' FunScript.txt
+
+echo "10. All the lines with the word zeus followed by space :"
+grep 'zeus ' FunScript.txt
+
+echo "11. All the line that the word programming appears at the end of the line :"
+grep 'programming$' FunScript.txt
+
+echo "12. All the lines that the word Fall appears at the beginning of the sentence :"
+grep '^Fall' FunScript.txt
+
+echo "13. All lines that have a word that starts with C and ends with 0 :"
+grep -E '\bC[[:alnum:]]*0\b' FunScript.txt
+
+echo "14. All lines that contain a word that starts with s and end with l :"
+grep -E '\bs[[:alpha:]]*l\b' FunScript.txt
