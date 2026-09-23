780bedeee086ca56db50b88547f9c7d9b7e708ae
Matthew Braziel <bmattcole@gmail.com>
2026-09-23 12:06:45 -0500

    Add FunScript file creation

--- HW3_MatthewBraziel.sh
+++ HW3_MatthewBraziel.sh
@@ -6,3 +6,17 @@
 # Assignment Number: 3
 # Due Date: 10/25/2026
 
+echo "Shell Programming By Matthew Braziel"
+
+echo "1. Creating a new File FunScript.txt."
+
+touch FunScript.txt
+cat > FunScript.txt << EOF
+Fall 2026
+CS4350
+Section #
+001
+scripts and shell programming using
+zeus ide
+@ TEXAS STATE UNIVERSITY
+EOF
